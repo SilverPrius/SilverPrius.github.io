12fb67b69eafb6a8e807673b7a62b1d4b9e0a5f8
Long Hoang <long88hoang@gmail.com>
2022-11-09 12:07:39 -0600

    Added: Pieces change color when player's turn changes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //create a variable for #board in .js file
 const board = document.querySelector("#board");
 
+const RED_TURN = 1;
+const YELLOW_TURN = 2;
+
 // 0 - empty, 1 - red, yellow - 2
 // this is an array that represents empty spaces and pieces on the board. It will be continuously updated as the game progresses
 const pieces = [
@@ -12,7 +15,8 @@ const pieces = [
     0, 0, 0, 0, 0, 0, 0,
 ];
 
-let playerTurn = 1; // 1 - red, 2 - yellow
+let playerTurn = RED_TURN; // 1 - red, 2 - yellow
+let floaterColumn = -1;
 
 //cell class
 //creating 42 cell divs and appending them to the board
@@ -45,29 +49,45 @@ function onCloumnClicked(column) {
     pieces[(availableRow * 7) + column] = playerTurn;
     let cell = board.children[(availableRow * 7) + column];
 
-    //create a new piece in our mouse entered. but now placed will equal true. add piece to the last empty index which is at the bottom of the board.
+    //Dropping a piece - create a new piece in our mouse entered. but now placed will equal true. Drop piece to the last empty index which is the last empty space at the bottom of the column. (append piece to that cell div and update the piece array with a '1' or '2' in the index array.)
     let piece = document.createElement("div");
     piece.className = "piece";
     piece.dataset.placed = true;
     piece.dataset.player = playerTurn;
     cell.appendChild(piece);
+
+    //Switch to next player's turn after a piece has been dropped
+    if(playerTurn === RED_TURN) {
+        playerTurn = YELLOW_TURN;
+    }else{
+        playerTurn = RED_TURN;
+    }
+
+    //changing floating piece's color after dropping a piece
+    changeColorFloater();
 }
 
-//function for what happens when mouse is hovered over the board
-function onMouseEnteredColumn(column) {
-
-    //remove pieces from the board if they are unplaced
+    function changeColorFloater() {
+        //remove pieces from the board if they are unplaced
     let unplacedPiece = document.querySelector("[data-placed='false']");
     if (unplacedPiece) {
         unplacedPiece.parentElement.removeChild(unplacedPiece)
     }
 
     //function to create a piece and place it in the selected column in the board
-    let cell = board.children[column];
+    let cell = board.children[floaterColumn];
     let piece = document.createElement("div");
     piece.className = "piece";
     piece.dataset.placed = false;
+    piece.dataset.player = playerTurn;
     cell.appendChild(piece);
+    }
+
+
+//function for what happens when mouse is hovered over the board
+function onMouseEnteredColumn(column) {
+    floaterColumn = column;
+    changeColorFloater();
 
 }
 
